ddf790612635f701165847e36f32d2368ee17ed8
Tatyana Lepestova <tatiana.lepestova@gmail.com>
2019-03-14 10:42:50 +0300

    ex2_js-basics-part2: task-01.js - Function processes the type of passed value. task-02.js - Function iterates over the elements of given array and returns it's length. task-03.js - Function counts and return odd, even and zero items. task-04.js - Function returns result of check for uniformity of items. task-05.js - Function returns the maximum value from items of given array. task-06.js - Function returns result of check of taken value for simpleness if it not greater than 1000.

---
+++ src/ex2_js-basics-part2/task-02.js
@@ -0,0 +1,10 @@
+function loopObject (arr) {
+    var quantity = arr.length - 1;
+
+    for (var i = 0; i <= quantity; i++) {
+        console.log( arr[i] );
+    }
+    console.log( "Число элементов: " + arr.length);
+}    
+
+module.exports = loopObject;
---
+++ src/ex2_js-basics-part2/task-06.js
@@ -0,0 +1,28 @@
+function checkIfSimple (num) { 
+    var message;
+
+    if (num < 2 || num > 1000) {
+        message = "Данные неверны" ; 
+    } else if (num === 2) {
+        message = "Число " + num + " - простое число"; 
+    } else {
+        
+        for (i = 2; i < num ; i++) {
+
+            if (num % i === 0) {
+                message = "Число " + num + " - составное число"; 
+                break;                       
+            } 
+            else {
+
+                if ( ++i === num) {                    
+                    message ="Число " + num + " - простое число";
+                }                
+                continue;
+            }             
+        }                
+    } 
+    return message      
+}
+
+module.exports = checkIfSimple;
