1b954a4394effb17e42347d27fbf29415eea175a
Tatyana Lepestova <tatiana.lepestova@gmail.com>
2019-03-27 18:19:09 +0300

    added checks for atribute value

--- src/ex5_js-functions/task.js
+++ src/ex5_js-functions/task.js
@@ -1,29 +1,35 @@
-var сalculator = {
+var Calculator = {
     result: 0,
     add: function add(num) {
-        сalculator.result += num;
+        if (Number.isFinite(num)) {
+            Calculator.result += num;
+        }
         return add;
     },
     subtract: function subtract(num) {
-        сalculator.result -= num;
+        if (Number.isFinite(num)) {
+            Calculator.result -= num;
+        }
         return subtract;
     },
     divide: function divide(num) {
-        сalculator.result /= num;
+        if (Number.isFinite(num)) {
+            Calculator.result /= num;
+        }
         return divide;
     },
     multiply: function multiply(num) {
-        сalculator.result *= num;
+        if (Number.isFinite(num)) {
+            Calculator.result *= num;
+        }
         return multiply;
     },
     getResult: function getResult() {
-
-        return сalculator.result
+        return Calculator.result
     },
     reset: function reset() {
-        сalculator.result = 0
+        Calculator.result = 0
         return reset;
     },
 }
-
-module.exports = calculator;
+module.exports = Calculator;
