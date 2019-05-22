966c5da79621b90e85ac0213845ad21e190f8e22
Tatyana Lepestova <tatiana.lepestova@gmail.com>
2019-05-22 12:16:22 +0300

    Move css rules to the separate file (task-01, task-02) Task-03 Change event for contact's icons from click to hover; Remove frame for central part content; Redo slider: add "like icon and text; add hover and click events; Move media rules to separate file. Add extra space between central part and articles for mobile view; Create toggle function for gamburger menu and search-field for mobile view.

--- src/ex10_html-css-fundamentals-part2/task-03.js
+++ src/ex10_html-css-fundamentals-part2/task-03.js
@@ -3,15 +3,20 @@
 var gamburger = document.getElementById("gamburger-icon");
 var dropdownMenu = document.getElementById("dropdown-menu");
 
-function showDropdownMenu(){
-    dropdownMenu.style.display= "block"; 
+function toggleDropdownMenu(){     
+    if(dropdownMenu.style.display === "block") {
+        dropdownMenu.style.display = "none";
+    }
+    else {
+        dropdownMenu.style.display= "block"; 
+    }    
 }
 
 function hideDropdownMenu(){
     var width = window.innerWidth    
     if (width <= 1345) {        
         dropdownMenu.style.display= "none"; 
-    }    
+    }     
 }
 /**************** Search icon and search field ***********************/
 
@@ -20,8 +25,15 @@ var searchForm = document.getElementById("search-form");
 var searchField = document.getElementById("search-field");
 var searchIcon = document.getElementById("search-icon");
 
-function showSearchForm(){
-    searchForm.style.display= "block"; 
+function toggleSearchForm(){    
+    if(searchForm.style.display === "block" ) {
+        searchForm.style.display = "none";
+        searchIcon.style.color = "#969798";
+    }
+    else if (searchForm.style.display=== "none" || searchForm.style.display=== "") {
+        searchForm.style.display = "block";
+        searchIcon.style.color = "#3687c4";       
+    }     
 }
 
 function searchIconChangeColor () {
@@ -33,7 +45,7 @@ function searchIconChangeColorBack () {
 }
 /**************** Contact icons and geolocation ***********************/
 
-var geoIcon = document.getElementById("geo");
+var contacts = document.getElementById("contacts");
 var popUp = document.getElementById("tooltip");
 
 function showGeoLocation() {
@@ -43,12 +55,28 @@ function showGeoLocation() {
 function hideGeoLocation() {
     popUp.style.display= "none";    
 }
+
+/***************************** Sliders *******************************/
+var sliderContainer = document.getElementById("slider-container");
+
+function toggleHeart(event){
+    var target = event.target;  
+    if (target.tagName != 'I') return;
+
+    if(target.attributes[0].nodeValue.includes("far")) {
+        target.attributes[0].nodeValue ="fas fa-heart";       
+    }
+    else {target.attributes[0].nodeValue ="far fa-heart";   
+    } 
+}
+
 /************************ EVENT LISTENERS ****************************/
 
-searchContainer.addEventListener("click", showSearchForm);
-gamburger.addEventListener("click", showDropdownMenu);
+searchIcon.addEventListener("click", toggleSearchForm);
+gamburger.addEventListener("click", toggleDropdownMenu);
 dropdownMenu.addEventListener("mouseleave", hideDropdownMenu);
 searchField.addEventListener("focus", searchIconChangeColor);
 searchField.addEventListener("blur", searchIconChangeColorBack);
-geoIcon.addEventListener("click", showGeoLocation);
-geoIcon.addEventListener("mouseout", hideGeoLocation);
+contacts.addEventListener("mouseover", showGeoLocation);
+contacts.addEventListener("mouseout", hideGeoLocation);
+sliderContainer.addEventListener("click", toggleHeart);
